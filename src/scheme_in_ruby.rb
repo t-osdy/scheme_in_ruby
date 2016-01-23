32d088fd88db790104b33cd66ee27f36865ddef6
tosada <tomoya.a.osada@gmail.com>
2016-01-24 00:13:59 +0900

    edit eval

--- src/scheme_in_ruby.rb
+++ src/scheme_in_ruby.rb
@@ -1,16 +1,40 @@
 ### evaluation ###
-def _eval(exp)
+def _eval(exp, env)
   if not list?(exp)
     if immediate_val?(exp)
       exp
     else
-      lookup_primitive_fun(exp)
+      lookup_var(exp, env)
     end
   else
-    fun = _eval(car(exp))
-    args = eval_list(cdr(exp))
-    apply(fun, args)
+    if special_form?(exp)
+      eval_special_form(exp, env)
+    else
+      fun = _eval(car(exp), env)
+      args = eval_list(cdr(exp), env)
+      apply(fun, args)
+    end
   end
+end
+
+def special_form?(exp)
+  lambda?(exp) or let?(exp)
+end
+
+def lambda?(exp)
+  exp[0] == :lambda
+end
+
+def eval_special_form(exp, env)
+  if lambda?(exp)
+    eval_lambda(exp, env)
+  elsif let?(exp)
+    eval_let(exp, env)
+  end
+end
+
+def eval_list(exp, env)
+  exp.map{|e| _eval(e, env)}
 end
 
 def list?(exp)
@@ -35,10 +59,6 @@ def cdr(list)
   list[1..-1]
 end
 
-def eval_list(exp)
-  exp.map{|e| _eval(e)}
-end
-
 def immediate_val?(exp)
   num?(exp)
 end
@@ -48,7 +68,15 @@ def num?(exp)
 end
 
 def apply(fun, args)
-  apply_primitive_fun(fun, args)
+  if primitive_fun?(fun)
+    apply_primitive_fun(fun, args)
+  else
+    lambda_apply(fun, args)
+  end
+end
+
+def primitive_fun?(exp)
+  exp[0] == :prim
 end
 
 def apply_primitive_fun(fun, args)
@@ -108,5 +136,8 @@ def closure_to_parameters_body_env(closure)
 end
 
 
-
-puts _eval([:+, [:+, 1, 2], 3])
+### output ###
+$global_env = [$primitive_fun_env]
+exp = [[:lambda, [:x, :y], [:+, :x, :y]], 3, 2]
+puts _eval(exp, $global_env)
+#puts _eval([:+, [:+, 1, 2], 3])
